bd270271c196b3c3ae90ab944c601ee28bad856b
Diego Espindola <diegoespindola9404@gmail.com>
2025-08-29 15:45:09 -0300

    adiciona botão de download

--- src/app/_components/navbar/navbar.ts
+++ src/app/_components/navbar/navbar.ts
@@ -8,14 +8,6 @@ import { RouterLink, RouterModule } from '@angular/router';
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
-export class Navbar implements OnInit {
-  ngOnInit(): void {
-    this.mensagem();
-  }
-
-
-  mensagem(){
-    console.log('Navbar iniciou');
-  }
+export class Navbar {
 
 }
